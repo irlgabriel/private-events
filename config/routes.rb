3a33c7c0be67f901792aa74d0450d36719dd11a5
irlgabriel <radu.gabriel131@gmail.com>
2020-07-19 21:05:06 +0300

    generated devise user controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :events
   post 'join', to:"events#join_event"
   post 'unjoin', to:"events#unjoin_event"
-  resources :users, only: :show
+  get '/user/:id', action: :show, controller: 'users'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "events#index"
 end
